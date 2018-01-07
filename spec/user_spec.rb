29fc2a2fda4f76fdbb899da767a409d6450eb8b1
chRIS <krzysiujozefczyk@wp.pl>
2018-01-07 20:57:07 +0100

    Add user ID validation spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper'
 RSpec.describe User do
   describe '.new' do
     subject(:valid_user) { User.new 1, 'John', 'Doe', '90010198765' }
-
+    subject(:user_with_wrong_id) { User.new 'A', 'John', 'Doe', '90010198765' }
     subject(:user_with_wrong_pesel) { User.new 1, 'John', 'Doe', '90010198a65' }
 
     it 'initializes a new valid user' do
       expect { valid_user }.not_to raise_error
     end
 
+    it 'raises error on user with wrong id' do
+      expect { user_with_wrong_id }.to raise_error(ArgumentError)
+    end
     it 'raises error on user with wrong pesel' do
       expect { user_with_wrong_pesel }.to raise_error(ArgumentError)
     end
