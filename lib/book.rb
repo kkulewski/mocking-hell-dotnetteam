128a3ef47c219886f70c4c493d307b27033160dd
chRIS <krzysiujozefczyk@wp.pl>
2018-01-07 21:51:38 +0100

    Add author validation implementation

--- lib/book.rb
+++ lib/book.rb
@@ -11,11 +11,17 @@ class Book
 
   def validate
     raise ArgumentError, 'Invalid ID' unless valid_id?
+    raise ArgumentError, 'Invalid author' unless valid_author?
     raise ArgumentError, 'Invalid year' unless valid_year?
   end
 
   def valid_id?
     @id.is_a?(Numeric)
+  end
+
+  def valid_author?
+    @author.length >= 1 && @author.length <= 50
+    @author.chars.first.upcase
   end
 
   def valid_year?
