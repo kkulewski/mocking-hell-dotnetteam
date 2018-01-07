7c6ecc889de3b6ca90ae38799bbdd57acc2a8de1
chRIS <krzysiujozefczyk@wp.pl>
2018-01-07 21:59:35 +0100

    Add title validation implementation

--- lib/book.rb
+++ lib/book.rb
@@ -12,6 +12,7 @@ class Book
   def validate
     raise ArgumentError, 'Invalid ID' unless valid_id?
     raise ArgumentError, 'Invalid author' unless valid_author?
+    raise ArgumentError, 'Invalid title' unless valid_title?
     raise ArgumentError, 'Invalid year' unless valid_year?
   end
 
@@ -20,8 +21,13 @@ class Book
   end
 
   def valid_author?
+    @author.is_a?(String)
     @author.length >= 1 && @author.length <= 50
     @author.chars.first.upcase
+  end
+
+  def valid_title?
+    @title.length >= 1 && @title.length <= 100
   end
 
   def valid_year?
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,19 +4,24 @@ RSpec.describe Book do
   describe '.new' do
     subject(:valid_book) { Book.new 1, 'Donald Knuth', 'The Art of Computer Programming', '1968' }
     subject(:book_with_wrong_id) { Book.new 'A', 'Donald Knuth', 'The Art of Computer Programming', '1968' }
-    subject(:book_with_wrong_author) { Book.new 'A', '', 'The Art of Computer Programming', '1968' }
+    subject(:book_with_wrong_author) { Book.new 1, '', 'The Art of Computer Programming', '1968' }
+    subject(:book_with_wrong_title) { Book.new 1, 'Donald Knuth', '', '1968' }
     subject(:book_with_wrong_year) { Book.new 1, 'Donald Knuth', 'The Art of Computer Programming', '2019' }
 
     it 'initializes a new book' do
       expect { valid_book }.not_to raise_error
     end
 
+    it 'raises error on user with wrong id' do
+      expect { book_with_wrong_id }.to raise_error(ArgumentError)
+    end
+
     it 'raises error on user with wrong author' do
       expect { book_with_wrong_author }.to raise_error(ArgumentError)
     end
 
-    it 'raises error on user with wrong id' do
-      expect { book_with_wrong_id }.to raise_error(ArgumentError)
+    it 'raises error on user with wrong title' do
+      expect { book_with_wrong_title }.to raise_error(ArgumentError)
     end
 
     it 'raises error on book with worng year' do
