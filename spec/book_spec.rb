92aefc70ede2c19aa7897ef7ff4e9dfde76b4ca3
chRIS <krzysiujozefczyk@wp.pl>
2018-01-07 21:51:15 +0100

    Add author validation spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -4,10 +4,15 @@ RSpec.describe Book do
   describe '.new' do
     subject(:valid_book) { Book.new 1, 'Donald Knuth', 'The Art of Computer Programming', '1968' }
     subject(:book_with_wrong_id) { Book.new 'A', 'Donald Knuth', 'The Art of Computer Programming', '1968' }
+    subject(:book_with_wrong_author) { Book.new 'A', '', 'The Art of Computer Programming', '1968' }
     subject(:book_with_wrong_year) { Book.new 1, 'Donald Knuth', 'The Art of Computer Programming', '2019' }
 
     it 'initializes a new book' do
       expect { valid_book }.not_to raise_error
+    end
+
+    it 'raises error on user with wrong author' do
+      expect { book_with_wrong_author }.to raise_error(ArgumentError)
     end
 
     it 'raises error on user with wrong id' do
