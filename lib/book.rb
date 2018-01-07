77c78ca814c8f03bab0f2463de45d172d92e7859
chRIS <krzysiujozefczyk@wp.pl>
2018-01-07 21:41:23 +0100

    Add ID validation implementation

--- lib/book.rb
+++ lib/book.rb
@@ -10,7 +10,12 @@ class Book
   end
 
   def validate
+    raise ArgumentError, 'Invalid ID' unless valid_id?
     raise ArgumentError, 'Invalid year' unless valid_year?
+  end
+
+  def valid_id?
+    @id.is_a?(Numeric)
   end
 
   def valid_year?
